cb56a666f794d738d825a01f29dcd45549d6ff00
Bradley Bossard <bradleybossard@gmail.com>
2018-08-31 06:40:19 -0700

    Replace ReactDOM with custom renderer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import ReactDOM from './renderer.js';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
